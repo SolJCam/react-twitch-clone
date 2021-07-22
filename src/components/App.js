e5056a0c9af40341e03a00c3d9b38050b2f4c3af
SolJCam <scameron110@gmail.com>
2021-07-21 23:04:34 -0400

    added 5 new components for app

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
 import React from "react";
 import { BrowserRouter, Router } from "react-router-dom";
+// import StreamCreate from './streams/StreamCreate';
+// import StreamDelete from './streams/StreamDelete';
+// import StreamEdit from './streams/StreamEdit';
+// import StreamList from './streams/StreamList';
+// import StreamShow from './streams/StreamShow';
+import * as streams from './streams';
 
 const App = () => {
     return(
-        <div>App</div>
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Router path="/" exact component={ streams.SteamList } />
+                    <Router path="/streams/new" component={ streams.SteamCreate } />
+                    <Router path="/streams/edit" component={ streams.SteamEdit } />
+                    <Router path="/streams/delete" component={ streams.SteamDelete } />
+                    <Router path="/streams/show" component={ streams.SteamShow } />
+                </div>
+            </BrowserRouter>
+        </div>
     );
 };
 
