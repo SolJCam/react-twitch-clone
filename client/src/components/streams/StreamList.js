8dfa7a1aed241a5261e31119350687f901eb5bea
SolJCam <scameron110@gmail.com>
2021-08-09 09:52:23 -0400

    caught spelling error in StreamList component. Misspelled ComponentDidMount

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
 
 class StreamList extends React.Component {
-    compnentDidMount(){
+    componentDidMount(){
         this.props.fetchStreams();
     };
 
@@ -20,4 +20,5 @@ const mapStateToProps = state => {
     return{ streams: Object.values(state.streams) };
 }
 
+// export default connect(null, { fetchStreams })(StreamList);
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
