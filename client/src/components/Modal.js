60476f7a2fa8312e5acc5ab3bffaf586b7138443
SolJCam <scameron110@gmail.com>
2021-08-14 22:06:55 -0400

    customized Modal and added background click behavior

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import history from '../history';
 
 const Modal = props => {
     return ReactDOM.createPortal(
-        <div className='ui dimmer modals visible active'>
-            <div className='ui standard modal visible active'>This is a Modal</div>
-        </div>
+        <div onClick={ () => history.push('/') } className='ui dimmer modals visible active'>
+            <div onClick={ (e) => e.preventPropogation() } className='ui standard modal visible active'> {/* preventPropogation: stop onClick actions in child divs from bubblng up to parent and executing */}
+                <div className='header'>Delete Stream</div>
+                <div className='content'>Are you sure you want to delete this stream?</div>
+                <div className='actions'>
+                    <button className='ui primary button'>Delete</button>
+                    <button className='ui button'>Cancel</button>
+                </div>
+            </div>
+        </div>,
+        document.querySelector("#Modal")
     );
 };
 
