2635aad3e5d08415f7d94b9a5d6f9d0930d3bf2a
SolJCam <scameron110@gmail.com>
2021-08-14 22:25:03 -0400

    removed hard coded text and buttons so that modal can be reusable

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -2,10 +2,24 @@ import React from 'react';
 import Modal from '../Modal';
 
 const StreamDelete = () => {
+    
+            // React.Fragment operates like an invisble ele having no impact on the dom. 
+            // We use it here in place of nesting divs which produced unusual display effects.
+    const actions = (
+        <React.Fragment>
+            <button className='ui button negative'>Delete</button>
+            <button className='ui button'>Cancel</button>
+        </React.Fragment>
+    )
+
     return(
         <div>
             StreamDelete
-            <Modal />
+            <Modal 
+                title="Delete Stream"
+                content="Are you sure you want to delete this stream?"
+                actions={ actions }
+            />
         </div>
     );
 };
