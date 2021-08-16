063088d548e10550647a8030548ecc84b8d56529
SolJCam <scameron110@gmail.com>
2021-08-16 00:06:04 -0400

    delete buttons now work to successfully navigate user to modal at the url for that streams id

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 const Modal = props => {
     return ReactDOM.createPortal(
         <div onClick={ props.onDismiss } className='ui dimmer modals visible active'>
-            <div onClick={ (e) => e.preventPropogation() } className='ui standard modal visible active'> {/* preventPropogation: stop onClick actions in child divs from bubblng up to parent and executing */}
+            <div onClick={ (e) => e.stopPropagation() } className='ui standard modal visible active'> {/* preventPropogation: stop onClick actions in child divs from bubblng up to parent and executing */}
                 <div className='header'>{ props.title }</div>
                 <div className='content'>{ props.content }</div>
                 <div className='actions'>{ props.actions }</div>
