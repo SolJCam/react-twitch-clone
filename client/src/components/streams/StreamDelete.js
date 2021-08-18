01846404f84726aeaa4417e499699c0e964d9b89
SolJCam <scameron110@gmail.com>
2021-08-18 15:30:10 -0400

    refactored StreamDelete component form function to class. Can now catch stream id upon load through ComponentDidMount

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,29 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Modal from '../Modal';
 import history from '../../history';
+import { fetchStream } from '../../actions';
 
-const StreamDelete = () => {
+class StreamDelete extends React.Component {
+    componenDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
     
+    renderActions () {
             // React.Fragment operates like an invisble ele having no impact on the dom. 
             // We use it here in place of nesting divs which produced unusual display effects.
-    const actions = (
-        <React.Fragment>
-            <button className='ui button negative'>Delete</button>
-            <button className='ui button'>Cancel</button>
-        </React.Fragment>
-    )
+        return(
+            <React.Fragment>
+                <button className='ui button negative'>Delete</button>
+                <button className='ui button'>Cancel</button>
+            </React.Fragment>
+        );
+    }
 
-    return(
-        <div>
-            StreamDelete
-            <Modal 
-                title="Delete Stream"
-                content="Are you sure you want to delete this stream?"
-                actions={ actions }
-                onDismiss={ ()=> history.push('/') }
-            />
-        </div>
-    );
+    render () {
+        return(
+            <div>
+                StreamDelete
+                <Modal 
+                    title="Delete Stream"
+                    content="Are you sure you want to delete this stream?"
+                    actions={ this.renderActions() }
+                    onDismiss={ ()=> history.push('/') }
+                />
+            </div>
+        );
+    }
 };
 
-export default StreamDelete;
+export default connect(null, { fetchStream })(StreamDelete);
