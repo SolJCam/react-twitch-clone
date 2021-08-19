488cdbf2d7c1969ff412797c21ea16d6da34b6a5
SolJCam <scameron110@gmail.com>
2021-08-19 00:58:30 -0400

    Added functionality to Delete and Cancel buttons

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Modal from '../Modal';
 import history from '../../history';
-import { fetchStream } from '../../actions';
+import { fetchStream, deleteStream } from '../../actions';
 
 class StreamDelete extends React.Component {
     componentDidMount() {
-        console.log(this.props);
+        // console.log(this.props);
         this.props.fetchStream(this.props.match.params.id);
     }
     
     renderActions () {
+        const { id } = this.props.match.params;
             // React.Fragment operates like an invisble ele having no impact on the dom. 
             // We use it here in place of nesting divs which produced unusual display effects.
         return(
             <React.Fragment>
-                <button className='ui button negative'>Delete</button>
-                <button className='ui button'>Cancel</button>
+                <button onClick={ () => this.props.deleteStream(id) } className='ui button negative'>Delete</button>
+                <Link to="/" className='ui button'>Cancel</Link>
             </React.Fragment>
         );
     }
@@ -45,4 +47,4 @@ const mapStateToProps = ( state, ownProps ) => {
     return { stream: state.streams[ownProps.match.params.id] }
 }
 
-export default connect(mapStateToProps, { fetchStream })(StreamDelete);
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
